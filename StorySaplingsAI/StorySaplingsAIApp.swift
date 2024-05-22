f9d412230e42cb7bb2f149b3806f80de1b73d949
Adriano Souza Costa <adriano.souza@prodoctor.net>
2024-05-22 09:54:44 -0300

    First commit

---
+++ StorySaplingsAI/StorySaplingsAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorySaplingsAIApp.swift
+//  StorySaplingsAI
+//
+//  Created by ProDoctor on 22/05/24.
+//
+
+import SwiftUI
+
+@main
+struct StorySaplingsAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
